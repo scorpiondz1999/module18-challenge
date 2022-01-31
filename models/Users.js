6aebd57577b973ae6e4c8e3f945381e334b67370
Djamel Ramdani <enzonpapa@gmail.com>
2022-01-30 18:02:58 -0600

    commit

---
+++ models/Users.js
@@ -0,0 +1,49 @@
+// Require Mongoose
+const { Schema, model } = require("mongoose");
+
+const UsersSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// total friends
+UsersSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Users model using the Users Schema
+const Users = model("Users", UsersSchema);
+
+// Export Users module
+module.exports = Users;
